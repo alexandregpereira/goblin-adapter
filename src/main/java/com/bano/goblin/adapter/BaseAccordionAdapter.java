ea3b038b01f6706e98fa7f56b3a3c8a50cf87092
Alexandre <alexandre.gomes2606@gmail.com>
2017-06-20 16:49:17 -0300

    Separate the class AccordionContainer from BaseAccordionAdapter

--- src/main/java/com/bano/goblin/adapter/BaseAccordionAdapter.java
+++ src/main/java/com/bano/goblin/adapter/BaseAccordionAdapter.java
@@ -2,24 +2,17 @@ package com.bano.goblin.adapter;
 
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
-import android.support.annotation.IntDef;
 import android.support.annotation.LayoutRes;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 
 /**
  *
  * Provides a accordion implementation
  */
-
-public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends BaseAdapter<BaseAccordionAdapter.AccordionContainer<T>, E>{
-
-    private static final int TYPE_ITEM = 1;
-    private static final int TYPE_CATEGORY = 2;
+public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends BaseAdapter<AccordionContainer<T>, E>{
 
     private final boolean mExpandOnlyOneFlag;
     private AccordionContainer<T> mPreviousAccordionOpened;
@@ -41,7 +34,7 @@ public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends
         return new BaseAdapter.ViewHolder<>(binding, new OnClickListener<AccordionContainer<T>>() {
             @Override
             public void onClicked(AccordionContainer<T> accordionContainer) {
-                if(accordionContainer.type == TYPE_CATEGORY) {
+                if(accordionContainer.type == AccordionContainer.TYPE_CATEGORY) {
                     if(accordionContainer.isOpen()){
                         collapse(accordionContainer);
                     }
@@ -60,9 +53,9 @@ public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends
     private void expand(AccordionContainer<T> accordionContainer){
         mPreviousAccordionOpened = accordionContainer;
         accordionContainer.setOpen(true);
-        int position = accordionContainer.position + 1;
+        int position = accordionContainer.getPosition() + 1;
         for (T t : accordionContainer.tList) {
-            addItem(position, new AccordionContainer<>(t, TYPE_ITEM));
+            addItem(position, new AccordionContainer<>(t));
             ++position;
         }
 
@@ -74,10 +67,10 @@ public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends
     private void collapse(AccordionContainer<T> accordionContainer){
         accordionContainer.setOpen(false);
         for (T t : accordionContainer.tList) {
-            remove(new AccordionContainer<>(t, TYPE_ITEM));
+            remove(new AccordionContainer<>(t));
         }
 
-        for(int i = accordionContainer.position + 1; i < getItems().size(); i++){
+        for(int i = accordionContainer.getPosition() + 1; i < getItems().size(); i++){
             getItems().get(i).setPosition(i);
         }
     }
@@ -88,10 +81,10 @@ public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends
         accordionContainer.setPosition(position);
         holder.binding.getRoot().setTag(accordionContainer);
         switch (accordionContainer.type){
-            case TYPE_CATEGORY:
+            case AccordionContainer.TYPE_CATEGORY:
                 this.onCategoryBindViewHolder(holder.binding, accordionContainer.category);
                 break;
-            case TYPE_ITEM:
+            case AccordionContainer.TYPE_ITEM:
                 this.onItemBindViewHolder(holder.binding, accordionContainer.t);
                 break;
         }
@@ -100,63 +93,5 @@ public abstract class BaseAccordionAdapter<T, E extends ViewDataBinding> extends
     @Override
     protected void onBindViewHolder(E e, AccordionContainer<T> accordionContainer) {
 
-    }
-
-    public static class AccordionContainer<T>{
-        @IntDef(value = {TYPE_ITEM, TYPE_CATEGORY})
-        @Retention(RetentionPolicy.SOURCE)
-        private @interface TypeFlags {}
-
-        private final String category;
-        public final T t;
-        private final ArrayList<T> tList;
-        public final int type;
-        private int position;
-        private boolean open;
-
-        public AccordionContainer(String category, ArrayList<T> tList) {
-            this.tList = tList;
-            this.type = TYPE_CATEGORY;
-            this.category = category;
-            this.t = null;
-        }
-
-        private AccordionContainer(T t, @TypeFlags int type) {
-            this.category = null;
-            this.t = t;
-            this.tList = null;
-            this.type = type;
-        }
-
-        public int getPosition() {
-            return position;
-        }
-
-        public void setPosition(int position) {
-            this.position = position;
-        }
-
-        private boolean isOpen() {
-            return open;
-        }
-
-        private void setOpen(boolean open) {
-            this.open = open;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            AccordionContainer<?> that = (AccordionContainer<?>) o;
-
-            return t != null ? t.equals(that.t) : that.t == null;
-        }
-
-        @Override
-        public int hashCode() {
-            return t != null ? t.hashCode() : 0;
-        }
     }
 }
